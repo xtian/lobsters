29a25027092af4d886ce25e7491ff57b5b7462d3
Christian Wesselhoeft <hi@xtian.us>
2018-09-20 09:44:48 -0600

    homepage_spec: feature -> context

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -5,14 +5,14 @@ require 'rails_helper'
 RSpec.feature 'Reading Homepage' do
   let!(:story) { create(:story) }
 
-  feature 'when logged out' do
+  context 'when logged out' do
     scenario 'reading a story' do
       visit '/'
       expect(page).to have_content(story.title)
     end
   end
 
-  feature 'when logged in' do
+  context 'when logged in' do
     let(:user) { create(:user) }
     before(:each) { stub_login_as user }
 
