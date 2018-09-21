ec9a5e031d473d60864227756003d63a322d1d91
Christian Wesselhoeft <hi@xtian.us>
2018-09-21 10:29:31 -0600

    DownvotedCommenters: Break long line

--- app/models/downvoted_commenters.rb
+++ app/models/downvoted_commenters.rb
@@ -65,7 +65,11 @@ class DownvotedCommenters
           count(distinct comments.id) as n_comments,
           count(distinct story_id) as n_stories,
           sum(downvotes) as n_downvotes")
-        .having('count(distinct comments.id) > 2 and count(distinct story_id) > 1 and sum(downvotes) >= 10')
+        .having('
+          count(distinct comments.id) > 2 and
+          count(distinct story_id) > 1 and
+          sum(downvotes) >= 10
+        ')
         .order('sigma desc')
         .limit(30)
         .each_with_object({}) do |u, hash|
