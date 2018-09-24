6100271da0e95fc829f7ce7512e4cf5f046bbb45
Christian Wesselhoeft <hi@xtian.us>
2018-09-24 14:23:06 -0600

    config/secrets.yml: Add mailgun_domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: Rails.application.secrets.mailgun_api_key,
-    domain: Rails.application.secrets.domain
+    domain: Rails.application.secrets.mailgun_domain
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
